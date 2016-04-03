3ba1b305b671f0edc7ba1471269b5812aa73017d
Felix Merk <felix@merkonline.com>
2016-04-02 23:57:31 -0400

    updated maps asks google for dist, clean loc data, doesn't crash after choosing dest

--- SafeWalk/app/src/main/java/com/safewalk/safewalk/MapsActivity.java
+++ SafeWalk/app/src/main/java/com/safewalk/safewalk/MapsActivity.java
@@ -1,31 +1,52 @@
 package com.safewalk.safewalk;
 
-//import android.app.Activity;
-//import android.app.Fragment;
-import android.app.Fragment;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.text.Html;
-import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import im.delight.android.location.SimpleLocation;
 
+package com.example.felix.princetonsafewalk;
 
-public class MapsActivity extends FragmentActivity /*implements OnMapReadyCallback */{
 
+
+public class MapsActivity extends FragmentActivity /*implements OnMapReadyCallback */ {
+    private String s_lat = "0";
+    private String s_long = "0";
+    private String d_lat = "0";
+    private String d_long = "0";
+    private String m_time = "0";
     //private GoogleMap mMap;
 
     private SimpleLocation location;
+
+    int PLACE_PICKER_REQUEST = 1;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,23 +71,9 @@ public class MapsActivity extends FragmentActivity /*implements OnMapReadyCallba
         final double latitude = location.getLatitude();
         final double longitude = location.getLongitude();
         System.out.println("lat" + latitude);
-        /*
-        int PLACE_PICKER_REQUEST = 1;
-        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+        s_lat = Double.toString(latitude);
+        s_long = Double.toString(longitude);
 
-        try {
-            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
-        } catch (GooglePlayServicesRepairableException e) {
-            e.printStackTrace();
-        } catch (GooglePlayServicesNotAvailableException e) {
-            e.printStackTrace();
-        }*/
-
-        // TODO
-        //    }
-
-        //});
-        int PLACE_PICKER_REQUEST = 1;
 
         try {
             PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
@@ -76,10 +83,93 @@ public class MapsActivity extends FragmentActivity /*implements OnMapReadyCallba
         } catch (GooglePlayServicesNotAvailableException e) {
             e.printStackTrace();
         }
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, this);
+                String toastMsg = String.format("Place: %s", place.getLatLng());
+                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
+                System.out.println(toastMsg);
+                toastMsg = toastMsg.replaceAll(".*\\(", "");
+                toastMsg = toastMsg.replaceAll("[() ]", "");
+                String[] marr = toastMsg.trim().split(",");
+                d_lat = marr[0];
+                d_long = marr[1];//Double.parseDouble(
+                System.out.println("DEST" + d_lat + "," + d_long);
+                new MyDownloadTask().execute();
+            }
+        }
 
-    /*@Override
+    }
+
+    class MyDownloadTask extends AsyncTask<Void,Void,Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            System.out.println("we stopped here");
+            System.out.println("lat,long,start check" + s_lat + "," + s_long);
+
+
+            StringBuilder urlString = new StringBuilder();
+            urlString.append("https://maps.googleapis.com/maps/api/distancematrix/json?");
+            urlString.append("&units=").append("imperial");
+            urlString.append("&mode=").append("walking");
+            urlString.append("&origins=").append(s_lat + "," + s_long);
+            urlString.append("&destinations=").append(d_lat + "," + d_long);
+            urlString.append("&key=").append("AIzaSyAzjrtTZCi2fW0XhVthMmSgQ_vUezAzb2A");
+            HttpURLConnection con = null;
+            URL url = null;
+            JSONObject object = null;
+            try {
+                url = new URL(urlString.toString());
+                System.out.println(url);
+
+                con = (HttpURLConnection) url.openConnection();
+                System.out.println("we stopped here 3");
+                con.connect();
+                System.out.println("we stopped here 4");
+                int status = con.getResponseCode();
+                System.out.println("we stopped here 5");
+
+                if (status >= 200 && status < 300) {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    String inputLine;
+                    StringBuffer response = new StringBuffer();
+
+                    while ((inputLine = br.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+                    br.close();
+                    System.out.println("we stopped here 6");
+                    //print result
+                    String ares = response.toString();
+                    System.out.println(ares);
+                    Pattern pattern = Pattern.compile("([0-9]+) mins");
+                    Matcher matcher = pattern.matcher(ares);
+                    if (matcher.find())
+                    {
+                        System.out.println(matcher.group(0));
+                        m_time = matcher.group(1);
+                    }
+
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("TEST of IOException error");
+            }
+            return null; //HISDFPSDPFHPSDP
+        }
+    }
+
+    @Override
     protected void onResume() {
 
 
@@ -99,25 +189,52 @@ public class MapsActivity extends FragmentActivity /*implements OnMapReadyCallba
         // ...
 
         super.onPause();
-    }*/
-
-    //@Override
-
-    /*public void onMapReady(GoogleMap googleMap) {
-
-    }*/
-
-    // TESTING
-    @Override
-    public void onStop() {
-        System.out.println("we stopped here");
-        super.onStop();
     }
+
+
     @Override
     public void onDestroy() {
         System.out.println("we destroyed here");
         super.onDestroy();
     }
 
-}
+    @Override
+    public void onStart() {
+        super.onStart();
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Maps Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.felix.princetonsafewalk/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Maps Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.felix.princetonsafewalk/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+}
